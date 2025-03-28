3d1b47af0f533436077f6f087e107ba26b6ba0d7
staceytoh <staceytohh@gmail.com>
2025-03-28 13:25:19 -0700

    Fixing middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,13 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function middleware(_: NextRequest) {
-  // Return the response without any modification
-  return NextResponse.next();
-}
+// Basic middleware setup with default behavior
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 }; 
